786f91e539d063f055bc5341d58887bbb403703e
Hoe Wei Leong <weileonghoe@gmail.com>
2026-02-16 11:53:29 +0800

    build(deps): add image_picker package

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,13 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.library")) {
+            dependencies {
+                add("implementation", "androidx.concurrent:concurrent-futures:1.1.0")
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
