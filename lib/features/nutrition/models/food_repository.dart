7acdc0d73249dacb38ed796c1acbf18751eeb50e
Hoe Wei Leong <weileonghoe@gmail.com>
2026-02-23 16:53:54 +0800

    feat: update food repository and food services

--- lib/features/nutrition/models/food_repository.dart
+++ lib/features/nutrition/models/food_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:kitahack_2026/features/nutrition/models/food_model.dart';
 import 'package:kitahack_2026/features/nutrition/models/food_services.dart';
 
@@ -10,7 +12,16 @@ class FoodRepository {
     try {
       await foodServices.saveFood(foods.userId, foods.toMap());
     } catch (e) {
-      throw Exception('Failed to Save User Data: $e');
+      throw Exception('Failed to Save Foods Data: $e');
     }
   }
+
+  // TODO : Wait for Nicholas
+  // Future<String> uploadFoodImage(String uid,Uint8List imageBytes) async {
+  //   try{
+  //     await foodServices.saveFoodImages(uid, imageBytes);
+  //   } catch (e) {
+  //     throw Exception('Failed to Save Food Image: $e');
+  //   }
+  // }
 }
--- lib/features/nutrition/models/food_services.dart
+++ lib/features/nutrition/models/food_services.dart
@@ -1,7 +1,11 @@
+import 'dart:typed_data';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class FoodServices {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
 
   // Future<DocumentSnapshot> getFoodData(String uid) async {
   //   return await _db.collection('food').doc(uid).get();
@@ -10,4 +14,10 @@ class FoodServices {
   Future<void> saveFood(String uid,Map<String,dynamic> rawData) async {
     await _db.collection('user').doc(uid).collection('foods').add(rawData);
   }
+
+  // TODO : Wait for Nicholas since Firebase Storage need Blaze Plan
+  // Future<String> saveFoodImages(String uid,Uint8List imageBytes) async {
+  //   await _storage.bucket();
+  //   return ;
+  // }
 }
