30a8bfb656095bc09c8990a75ea0211c377acd68
Hoe Wei Leong <weileonghoe@gmail.com>
2026-02-25 16:10:05 +0800

    feat: add history_summary_item to show logged meals

--- lib/features/history/presentation/widgets/history_summary_item.dart
+++ lib/features/history/presentation/widgets/history_summary_item.dart
@@ -1,33 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:kitahack_2026/core/theme/mango_theme.dart';
+import 'package:kitahack_2026/core/theme/sizes.dart';
 
-
-// TODO: to be replaced?
 class HistorySummaryItem extends StatelessWidget {
   const HistorySummaryItem({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
     return Container(
       margin: const EdgeInsets.only(bottom: 10),
       child: Material(
-        color: Colors.transparent,
+        borderRadius: BorderRadius.circular(kDefaultRadius),
+        color: colorScheme.primaryContainer,
         child: Ink(
           height: 100,
           decoration: BoxDecoration(
-            color: kPlaceholderGrey,
-            borderRadius: BorderRadius.circular(8),
+            borderRadius: BorderRadius.circular(kDefaultRadius),
           ),
           child: InkWell(
-            borderRadius: BorderRadius.circular(8),
-            splashColor: Colors.white.withValues(alpha: .3),
+            borderRadius: BorderRadius.circular(kDefaultRadius),
             onTap: () {
               Future.delayed(const Duration(milliseconds: 80), () {
                 if (!context.mounted) return;
                 Navigator.pushNamed(context, '/result');
               });
             },
-            child: const Center(),
+            child: Padding(
+              padding: const EdgeInsets.all(kDefaultPadding),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Mango Salad',
+                          style: textTheme.titleMedium?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: colorScheme.onSurface,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        kGap8,
+                        Text(
+                          '12:30 pm',
+                          style: textTheme.bodyMedium?.copyWith(
+                            color: colorScheme.onSurfaceVariant,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "420",
+                        style: textTheme.headlineSmall?.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: colorScheme.primary,
+                        ),
+                      ),
+                      Text(
+                        "KCAL",
+                        style: textTheme.labelMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: colorScheme.onSurfaceVariant,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
